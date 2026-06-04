05c1d530dbd2dc48005f7fa91f36835d2d7259d3
Nanhisinghmar <kajalsinghmar80@gmail.com>
2026-06-04 19:45:28 +0530

    Add files via upload
    Add calculator project

---
+++ calculator.py
@@ -0,0 +1,11 @@
+a=int(input('a'))
+b=int(input('b'))
+s=input('1 for sum ,2 for sub,3 for mul ')
+if s==1:
+    print(a+b)
+elif s==2:
+     print(a-b)
+elif s==3:
+    print(a*b)
+else:
+     print(a/b)
